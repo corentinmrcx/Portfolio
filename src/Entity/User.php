f8a11fdba4e0f965072534710f11e6677f838025
marc0237 <corentin.marcoux@etudiant.univ-reims.fr>
2025-03-04 22:26:46 +0100

    Regénération de l'entité User pour permettre l'authentification

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -3,42 +3,34 @@
 namespace App\Entity;
 
 use App\Repository\UserRepository;
-use Doctrine\Common\Collections\ArrayCollection;
-use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Security\Core\User\PasswordAuthenticatedUserInterface;
+use Symfony\Component\Security\Core\User\UserInterface;
 
 #[ORM\Entity(repositoryClass: UserRepository::class)]
 #[ORM\Table(name: '`user`')]
-class User
+#[ORM\UniqueConstraint(name: 'UNIQ_IDENTIFIER_EMAIL', fields: ['email'])]
+class User implements UserInterface, PasswordAuthenticatedUserInterface
 {
     #[ORM\Id]
     #[ORM\GeneratedValue]
     #[ORM\Column]
     private ?int $id = null;
 
-    #[ORM\Column(length: 255)]
+    #[ORM\Column(length: 180)]
     private ?string $email = null;
 
-    #[ORM\Column(length: 255)]
+    /**
+     * @var list<string> The user roles
+     */
+    #[ORM\Column]
+    private array $roles = [];
+
+    /**
+     * @var string The hashed password
+     */
+    #[ORM\Column]
     private ?string $password = null;
-
-    /**
-     * @var Collection<int, Project>
-     */
-    #[ORM\OneToMany(targetEntity: Project::class, mappedBy: 'users')]
-    private Collection $project;
-
-    /**
-     * @var Collection<int, Skill>
-     */
-    #[ORM\OneToMany(targetEntity: Skill::class, mappedBy: 'users')]
-    private Collection $skill;
-
-    public function __construct()
-    {
-        $this->project = new ArrayCollection();
-        $this->skill = new ArrayCollection();
-    }
 
     public function getId(): ?int
     {
@@ -57,6 +49,43 @@ class User
         return $this;
     }
 
+    /**
+     * A visual identifier that represents this user.
+     *
+     * @see UserInterface
+     */
+    public function getUserIdentifier(): string
+    {
+        return (string) $this->email;
+    }
+
+    /**
+     * @see UserInterface
+     *
+     * @return list<string>
+     */
+    public function getRoles(): array
+    {
+        $roles = $this->roles;
+        // guarantee every user at least has ROLE_USER
+        $roles[] = 'ROLE_USER';
+
+        return array_unique($roles);
+    }
+
+    /**
+     * @param list<string> $roles
+     */
+    public function setRoles(array $roles): static
+    {
+        $this->roles = $roles;
+
+        return $this;
+    }
+
+    /**
+     * @see PasswordAuthenticatedUserInterface
+     */
     public function getPassword(): ?string
     {
         return $this->password;
@@ -70,62 +99,11 @@ class User
     }
 
     /**
-     * @return Collection<int, Project>
+     * @see UserInterface
      */
-    public function getProject(): Collection
+    public function eraseCredentials(): void
     {
-        return $this->project;
-    }
-
-    public function addProject(Project $project): static
-    {
-        if (!$this->project->contains($project)) {
-            $this->project->add($project);
-            $project->setUsers($this);
-        }
-
-        return $this;
-    }
-
-    public function removeProject(Project $project): static
-    {
-        if ($this->project->removeElement($project)) {
-            // set the owning side to null (unless already changed)
-            if ($project->getUsers() === $this) {
-                $project->setUsers(null);
-            }
-        }
-
-        return $this;
-    }
-
-    /**
-     * @return Collection<int, Skill>
-     */
-    public function getSkill(): Collection
-    {
-        return $this->skill;
-    }
-
-    public function addSkill(Skill $skill): static
-    {
-        if (!$this->skill->contains($skill)) {
-            $this->skill->add($skill);
-            $skill->setUsers($this);
-        }
-
-        return $this;
-    }
-
-    public function removeSkill(Skill $skill): static
-    {
-        if ($this->skill->removeElement($skill)) {
-            // set the owning side to null (unless already changed)
-            if ($skill->getUsers() === $this) {
-                $skill->setUsers(null);
-            }
-        }
-
-        return $this;
+        // If you store any temporary, sensitive data on the user, clear it here
+        // $this->plainPassword = null;
     }
 }
